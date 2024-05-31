375c1e8459939513f72a5d95a7e531e19fbbdec8
lanarozova <svitlaroza@gmail.com>
2024-05-31 13:41:56 +0300

    styled login page with bootstrap template

---
+++ accounts/forms.py
@@ -0,0 +1,62 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+
+User = get_user_model()
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        max_length=255,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control form-control-lg",
+                "placeholder": "Enter a valid email address",
+            }
+        ),
+    )
+    password = forms.CharField(
+        max_length=255,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control form-control-lg",
+                "placeholder": "Enter password",
+            }
+        ),
+    )
+    remember_me = forms.BooleanField(
+        required=False,
+        initial=True,
+        widget=forms.CheckboxInput(attrs={"class": "form-check-input me-2"}),
+    )
+
+
+class RegisterForm(UserCreationForm):
+    agree_terms = forms.BooleanField(
+        required=True,
+        label="I agree all statements in Terms of service",
+        widget=forms.CheckboxInput(),
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "password1",
+            "password2",
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({"class": "form-control"})
+
+            if field_name == "agree_terms":
+                field.widget.attrs.update({"class": "form-check-input me-2"})
+
+            if self.errors.get(field_name):
+                field.widget.attrs["class"] += " is-invalid"
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,89 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.utils.encoding import iri_to_uri
+from django.utils.http import url_has_allowed_host_and_scheme
+from django.views.generic import FormView, CreateView, DeleteView, DetailView, ListView
+from accounts.models import User
+
+from accounts.forms import LoginForm, RegisterForm
+from config import settings
+
+
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 
 def home_view(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("<h1>Main page</h1>")
+    return render(request, "home.html")
+
+
+class RegisterView(FormView):
+    template_name = "accounts/register.html"
+    form_class = RegisterForm
+    success_url = reverse_lazy("accounts:login")
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            messages.info(request, "You are already logged in")
+            return redirect("accounts:home")
+        return super().get(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+
+        messages.info(
+            self.request,
+            "Registration completed. Please check your email to activate your account.",
+        )
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        messages.info(request, "You're already logged in")
+        return redirect("accounts:home")
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            remember_me = form.cleaned_data.get("remember_me")
+            username = User.objects.get(email=email.lower()).username
+            user = authenticate(request, username=username, password=password)
+            print(email, password, user)
+
+            if not user:
+                messages.error(request, "Invalid login or password")
+                return redirect("accounts:login")
+
+            if remember_me:
+                request.session.set_expiry(60 * 60 * 24 * 7)
+            else:
+                request.session.set_expiry(0)
+
+            login(request, user)
+
+            next_url = request.GET.get("next")
+            if next_url and url_has_allowed_host_and_scheme(
+                next_url, settings.ALLOWED_HOSTS
+            ):
+                return redirect(iri_to_uri(next_url))
+
+            return redirect("accounts:home")
+        else:
+            return render(request, "accounts/login.html", {"form": form})
+
+    form = LoginForm()
+    return render(request, "accounts/login.html", {"form": form})
+
